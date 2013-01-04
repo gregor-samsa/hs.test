fca55be6cd8da1a38d3bbad4fb777171af554e4f
Gregor Samsa <greg0r.samsa>
2013-01-03 21:45:34 -0800

    added type family with GADT & type promotion

--- promote.hs
+++ promote.hs
@@ -1,4 +1,18 @@
 {-# LANGUAGE GADTs, KindSignatures, DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+
+data Basica = 
+    BInt
+  | BChar
+
+type family F (a :: Basica) :: * 
+type instance F BInt  = Int
+type instance F BChar = Char
+
+-- data TestF :: F * -> * where -- doesn't work, type synonyms can't be promoted
+data TestF :: * -> * where
+  TInt  :: TestF (F BInt)
+  TChar :: TestF (F BChar)
 
 data Nat = Ze | Su Nat
 
@@ -12,3 +26,23 @@ data Type t where
 
 data Test :: Type Int -> * where
   A :: Test (RInt)
+  B :: Test (RInt)
+
+data Adt =
+    One
+  | Two
+  | Three 
+
+type SInt = Int
+{-- compile error
+      `SInt' of kind `*' is not promotable
+
+data TestAdt1 :: SInt -> * where 
+  Ab :: TestAdt1 (SInt)
+
+--}
+
+data TestAdt :: Adt -> * where
+  Aa :: TestAdt (One)
+  Bb :: TestAdt (Two)
+  Cc :: TestAdt (Three)
