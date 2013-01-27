780e7dd402cb21cccdcfb60e4016c8b6b836e8c8
Gregor Samsa <greg0r.samsa>
2013-01-27 12:08:11 -0800

    tagless experiment

--- iyd1.hs
+++ iyd1.hs
@@ -1,71 +1,18 @@
-{-# LANGUAGE GADTs, StandaloneDeriving #-}
-{-# LANGUAGE GADTs, ExistentialQuantification #-}
-{-# LANGUAGE KindSignatures, DataKinds #-}
-{-# LANGUAGE RankNTypes, EmptyDataDecls #-}
+{-# LANGUAGE GADTs, StandaloneDeriving, KindSignatures #-}
 
-data CameraMake = 
-    LeicaT
-  | CanonT
-  | NikonT
-  | PanasonicT
-    deriving Show
+data Exif1 :: * -> * where
+  Manufacturer  :: Exif1 String
+  Model         :: Exif1 String
+  Orientation   :: Exif1 String
+  IsoSpeed      :: Exif1 Int
+  Field         :: Exif1 Dynamic
 
-data NikonModel = 
-    D90
-    deriving Show
+type ExifFieldList = [Exif1 Dynamic]
 
-data PanasonicModel = 
-    Gf1
-  | Gf2
-    deriving Show
+-- TRep - tagless representation type
+data TRep :: * -> * where
+  TExifList  :: TRep  ExifFieldList -- check
+deriving instance Show (TRep a)
 
-data CanonModel = 
-    EOS
-  | Rebel
-    deriving Show
-
-data LeicaModel = 
-    M3
-  | M9
-  | S2
-    deriving Show
-
-data Camera :: CameraMake -> * where
-  Leica :: LeicaModel -> Camera LeicaT
-  Canon :: CanonModel -> Camera CanonT
-  Panasonic :: PanasonicModel -> Camera PanasonicT
-  Nikon :: Int -> NikonModel -> Camera NikonT
-deriving instance Show (Camera a)
-
-showCamera (Leica m) = show m
-showCamera (Canon m) = show m
-showCamera (Nikon n m) = show m ++ show n
-
-data Exif = Exif
-    deriving Show
-
--- TODO: make it tagless
-data Rep :: * -> * where
-  RInt    :: Int -> Rep Int
-  RChar   :: Int -> Rep Char
-  RString :: [Char] -> Rep String
-  ExifManufacturer :: String -> Rep Exif
-  ExifModel        :: Rep String -> Rep Exif 
-  ExifOrientation  :: Rep String -> Rep Exif
-  ExifIsoSpeed     :: Rep Int -> Rep Exif
-  ExifList         :: [Rep Exif] -> Rep [Exif]
-deriving instance Show (Rep a)
-
-showRep :: forall a. Rep a -> String
-showRep (RInt i)    = show i
-showRep (RChar c)   = show c
-showRep (RString s) = show s
-showRep (ExifManufacturer m) = showRep (RString m)
-showRep (ExifModel m)        = showRep m
-showRep (ExifOrientation m)  = showRep m
-showRep (ExifIsoSpeed m)     = showRep m
-showRep (ExifList (a:l))     = showRep a ++ showRep (ExifList l) 
-showRep (ExifList [])        = ""
-
-t = showRep (ExifList [ExifModel (RString "HI"), ExifIsoSpeed (RInt 3)])
-
+data Dynamic where 
+  Dyn :: TRep t -> t -> Dynamic 
