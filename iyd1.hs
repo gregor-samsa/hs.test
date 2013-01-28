8f69df8e6e89bbda111b8782a63aa27e8a9537f2
Gregor Samsa <greg0r.samsa>
2013-01-27 21:49:40 -0800

    fixes

--- iyd1.hs
+++ iyd1.hs
@@ -1,18 +1,26 @@
 {-# LANGUAGE GADTs, StandaloneDeriving, KindSignatures #-}
 
+data Field
+
 data Exif :: * -> * where
   Manufacturer  :: Exif String
   Model         :: Exif String
   Orientation   :: Exif String
   IsoSpeed      :: Exif Int
-  Field         :: Exif Dynamic
+  Field         :: (Show t) => Exif t -> t -> Exif Field 
+deriving instance Show (Exif a)
 
-type ExifFieldList = [Exif Dynamic]
+-- ImageFile (an image file with Exif)
+data ImageFile :: * -> * where
+  ExifList  :: ImageFile [(Exif Field)] 
+deriving instance Show (ImageFile a)
 
--- TRep - tagless representation type
-data TRep :: * -> * where
-  TExifList  :: TRep  ExifFieldList -- check
-deriving instance Show (TRep a)
-
+-- test
 data Dynamic where 
-  Dyn :: TRep t -> t -> Dynamic 
+  Dyn :: (Show t) => ImageFile t -> t -> Dynamic 
+deriving instance Show Dynamic
+
+f = Field Manufacturer "GF1"
+flist = Dyn ExifList [f,f,f]
+
+
