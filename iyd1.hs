b6e93c72c19514fae5c5daf2c1bf2fa6938fe1d2
Gregor Samsa <greg0r.samsa>
2013-02-01 22:43:52 -0800

    ugly exif first cut

--- iyd1.hs
+++ iyd1.hs
@@ -3,6 +3,7 @@ import System.Environment
 import System.Directory
 import System.FilePath.Find
 import System.FilePath.Posix as FilePath
+import qualified Graphics.Exif as Ex
 
 data Field
 
@@ -40,10 +41,32 @@ data File :: * where -- don't seem to need a gadt/phantom
  
 search pat dir = find always (fileName ~~? pat) dir
 
-printFileMeta f =
-  let (path, name) = FilePath.splitFileName f in
-  let ext = FilePath.takeExtension f in
-  let defaultf = DefaultFile (SimpleFileC name path f) in
+fromExif :: (String, String) -> ExifProperties -> ExifProperties
+fromExif ("Manufacturer", v) l = Field Manufacturer v : l
+fromExif ("Model", v) l        = Field Model v : l
+fromExif ("Orientation", v) l  = Field Orientation v : l
+fromExif ("IsoSpeed", v) l     = Field Manufacturer v : l  -- FIXME
+fromExif (_, v) l              = l
+
+
+mkSimpleFile f = SimpleFileC name path ext
+  where 
+    (path, name) = FilePath.splitFileName f 
+    ext = FilePath.takeExtension f 
+
+mkFile ".jpg" f = do
+  exif <- Ex.fromFile f
+  exiftags <- Ex.allTags exif
+  let fields = foldr fromExif [] exiftags
+  let sf = mkSimpleFile f
+  return $ ImageFile sf fields
+
+mkFile ext f = do
+  let (path, name) = FilePath.splitFileName f 
+  return $ DefaultFile $ mkSimpleFile f
+
+printFileMeta f = do
+  defaultf <- mkFile (FilePath.takeExtension f) f
   putStrLn (show defaultf)
  
 main = do [pat] <- getArgs
